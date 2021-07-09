97edc13b50e978d46da28c872c1e728534aa01ad
jkenzer <jkenzer@gmail.com>
2021-07-08 20:25:40 -0700

    Pull 15 posts

--- src/client/modules/data/reddit/reddit.js
+++ src/client/modules/data/reddit/reddit.js
@@ -1,10 +1,10 @@
 export const getSubReddit = async (subreddit) => {
-  subreddit = subreddit || "worldnews";
-  const resp = await (
-    await fetch(`https://www.reddit.com/r/${subreddit}.json`, {
-      mode: "cors",
-      headers: { Accept: "application/json" },
-    })
-  ).json();
-  return resp.data.children.slice(0, 10).map((c) => c.data);
+    subreddit = subreddit || 'worldnews';
+    const resp = await (
+        await fetch(`https://www.reddit.com/r/${subreddit}.json`, {
+            mode: 'cors',
+            headers: { Accept: 'application/json' }
+        })
+    ).json();
+    return resp.data.children.slice(0, 15).map((c) => c.data);
 };
