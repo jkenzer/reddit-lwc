b79bcb1df2140276b1e867a8f3926e24553ddc16
jkenzer <jkenzer@gmail.com>
2021-07-10 07:10:32 -0700

    Fix showing undefined on embedded media

--- src/client/modules/my/media/media.js
+++ src/client/modules/my/media/media.js
@@ -6,17 +6,24 @@ export default class Media extends LightningElement {
     embedContent = '';
 
     get hasEmbedMedia() {
+        if (
+            this.media.hasOwnProperty('media_embed') &&
+            this.media.media_embed.hasOwnProperty('content')
+        ) {
+            console.log('has key');
+        } else {
+            return false;
+        }
         const mydiv = this.template.querySelector('.embeddedDiv');
-        console.log(mydiv);
-        this.embedContent = this.media.media_embed?.content;
+        this.embedContent = this.media.media_embed.content;
         if (mydiv) {
             mydiv.innerHTML = this.htmlDecode(this.embedContent);
         }
-        return !this.media?.mediaDisabled;
+        return true;
     }
 
     get hasImageMedia() {
-        console.log(JSON.parse(JSON.stringify(this.media, undefined, 2)));
+        // console.log(JSON.parse(JSON.stringify(this.media, undefined, 2)));
         this.mediaURL = this.htmlDecode(
             this.media.preview?.images[0].resolutions[
                 this.media.preview?.images[0].resolutions.length - 1
